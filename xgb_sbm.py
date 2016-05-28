a8bd5da9ce37fb0096a1184486eeeeaa6ddcaf29
snakoneczny <szymon.nakoneczny@gmail.com>
2016-05-28 13:53:52 +0200

    xgb nrounds

--- xgb_sbm.py
+++ xgb_sbm.py
@@ -15,21 +15,21 @@ X_test = test
 
 # Define different targets
 targets = ['casual', 'registered']
+n_rounds = {CASUAL: 480, REGISTERED: 660}
 
 # Work with targets
 clf = {}
 y_pred = np.zeros(X_test.shape[0])
 for target in targets:
     # XGBoost matrices
-    xg_train = xgb.DMatrix(X_train, label=y_train[target])
-    xg_test = xgb.DMatrix(X_test)
+    xg_train = xgb.DMatrix(X_train.as_matrix(), label=y_train[target].as_matrix())
+    xg_test = xgb.DMatrix(X_test.as_matrix())
 
     # Train
     param = {'silent': 1, 'nthread': 8, 'objective': 'reg:linear',
              'eta': 0.01, 'max_depth': 10, 'min_child_weight': 2, 'colsample_bytree': 1,
              'subsample': 0.5, 'gamma': 0, 'alpha': 2, 'lambda': 2, 'lambda_bias': 0}
-    n_rounds = 520
-    clf[target] = xgb.train(param, xg_train, n_rounds, [(xg_train, 'train')], feval=rmsle_evalerror)
+    clf[target] = xgb.train(param, xg_train, n_rounds[target], [(xg_train, 'train')], feval=rmsle_evalerror)
 
     # Predict and clip
     y_pred += clf[target].predict(xg_test).clip(min=0)
