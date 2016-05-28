e9836751fff9152917b442a8528c26560d93bb30
snakoneczny <szymon.nakoneczny@gmail.com>
2016-05-28 13:22:19 +0200

    xgb cv

--- xgb_cv.py
+++ xgb_cv.py
@@ -2,12 +2,19 @@ from utils import *
 from sklearn.cross_validation import KFold
 import xgboost as xgb
 
+# Parameters
+features = EXTENDED
+
 # Read data
 train = pd.read_csv('data/train_extended.csv')
 
 # Get X and y
-X = train.drop(['casual', 'registered', 'count'], inplace=False, axis=1)
-y = train[['casual', 'registered', 'count']]
+X = train.drop([CASUAL, REGISTERED, COUNT], inplace=False, axis=1)
+y = train[[CASUAL, REGISTERED, COUNT]]
+
+# Define different targets
+targets = [CASUAL, REGISTERED]
+y_pred_all = {CASUAL: np.zeros(y.shape[0]), REGISTERED: np.zeros(y.shape[0])}
 
 # CV
 n_folds = 10
@@ -19,9 +26,6 @@ i = 0
 for train, test in skf:
     X_train, X_test = X.loc[train, :], X.loc[test, :]
     y_train, y_test = y.loc[train], y.loc[test]
-
-    # Define different targets
-    targets = ['casual', 'registered']
 
     # Work with targets
     clf = {}
@@ -40,18 +44,28 @@ for train, test in skf:
                                 feval=rmsle_evalerror, early_stopping_rounds=60)
 
         # Predict and clip values
-        y_pred += clf[target].predict(xg_test).clip(min=0)
+        y_pred_target = clf[target].predict(xg_test).clip(min=0)
+        y_pred += y_pred_target
+
+        # Save predictions
+        y_pred_all[target][test] = y_pred_target
 
     # Evaluate
-    rmsle_fold = rmsle(y_test['count'], y_pred)
+    rmsle_fold = rmsle(y_test[COUNT], y_pred)
     rmsle_mean += rmsle_fold / n_folds
-    best_round_casual[i] = clf['casual'].best_iteration
-    best_round_registered[i] = clf['registered'].best_iteration
+    best_round_casual[i] = clf[CASUAL].best_iteration
+    best_round_registered[i] = clf[REGISTERED].best_iteration
     i += 1
     print 'Fold %d/%d, RMSLE = %f, best it. = %d, %d' % (
-        i, n_folds, rmsle_fold, clf['registered'].best_iteration, clf['casual'].best_iteration)
+        i, n_folds, rmsle_fold, clf[CASUAL].best_iteration, clf[REGISTERED].best_iteration)
 
 # Show results
 print best_round_casual, best_round_registered
 print 'mean RMSLE = %f, ' % rmsle_mean
 print 'mean best it. = %f, %f' % (best_round_casual.mean(), best_round_registered.mean())
+
+# Write cross validation results
+date = pd.read_csv('data/train.csv')['datetime']
+submission = pd.DataFrame(
+    data={'datetime': date, CASUAL: y_pred_all[CASUAL], REGISTERED: y_pred_all[REGISTERED]})
+submission.to_csv('cross-validation/xgb_%s.csv' % features, index=False)
